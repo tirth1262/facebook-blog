e0743c84011a0157d2d3516afe40b0e0dc8f7568
tirths.inexture <tirths.inexture@gmail.com>
2022-07-01 19:05:11 +0530

    implemented login with github

--- blogapp/config.py
+++ blogapp/config.py
@@ -15,4 +15,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
+    GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
+    LINKED_CLIENT_ID = os.environ.get('LINKED_CLIENT_ID')
+    LINKED_CLIENT_SECRET = os.environ.get('LINKED_CLIENT_SECRET')
+    TWITTER_CLIENT_ID = os.environ.get('TWITTER_CLIENT_ID')
+    TWITTER_CLIENT_SECRET = os.environ.get('TWITTER_CLIENT_SECRET')
     PASSWORD = os.environ.get('PASSWORD')
+
--- blogapp/main/routes.py
+++ blogapp/main/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, Blueprint, flash, request, current_app, url_for, redirect
+from flask import (render_template, Blueprint, flash,
+                   request, current_app, url_for, redirect, session)
 from flask_login import current_user, login_required
 from blogapp.models import Post, User, UserProfile
 from sqlalchemy import desc
 from blogapp.decorators import count_friend_request
 from blogapp.helpers import friend_list, post_likes
-from blogapp import oauth,db
+from blogapp import oauth, db
 from flask_login import login_user
 
 main = Blueprint('main', __name__)
+
 
 google = oauth.register(
     name='google',
@@ -25,7 +27,6 @@ google = oauth.register(
     jwks_uri='https://www.googleapis.com/oauth2/v3/certs'
 )
 
-
 @main.route('/login/google')
 def google_login():
     google = oauth.create_client('google')
@@ -37,6 +38,7 @@ def google_login():
 @main.route('/login/google/authorize')
 def google_authorize():
     google = oauth.create_client('google')
+    print("----------->",google)
     google.authorize_access_token()
     resp = google.get('userinfo').json()
     user_email = resp['email']
@@ -54,6 +56,101 @@ def google_authorize():
         db.session.commit()
         # user = User.query.filter_by(email=resp['email']).first()
         profile = UserProfile(firstname=None, lastname=None, profile_image=resp['picture'], birthday=None,
+                              user_id=user_obj.id)
+        db.session.add(profile)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('main.home'))
+
+
+github = oauth.register(
+    name='github',
+    client_id=current_app.config["GITHUB_CLIENT_ID"],
+    client_secret=current_app.config["GITHUB_CLIENT_SECRET"],
+    access_token_url='https://github.com/login/oauth/access_token',
+    access_token_params=None,
+    authorize_url='https://github.com/login/oauth/authorize',
+    authorize_params=None,
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'openid email profile'},
+)
+
+# twitter=oauth.register(
+#     name='twitter',
+#     client_id=current_app.config["TWITTER_CLIENT_ID"],
+#     client_secret=current_app.config["TWITTER_CLIENT_SECRET"],
+#     api_base_url='https://api.twitter.com/1.1/',
+#     request_token_url='https://api.twitter.com/oauth/request_token',
+#     access_token_url='https://api.twitter.com/oauth/access_token',
+#     authorize_url='https://api.twitter.com/oauth/authenticate',
+#     # fetch_token=lambda: session.get('token'),  # DON'T DO IT IN PRODUCTION
+# )
+
+
+twitter=oauth.register(
+    name='twitter',
+    client_id=current_app.config['TWITTER_CLIENT_ID'],
+    client_secret=current_app.config['TWITTER_CLIENT_SECRET'],
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    request_token_params=None,
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    access_token_params=None,
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    authorize_params=None,
+    api_base_url='https://api.twitter.com/1.1/',
+    client_kwargs={'scope': 'openid'},
+
+
+
+)
+@main.route('/login/twitter')
+def twitter_login():
+    twitter = oauth.create_client('twitter')
+    redirect_uri = url_for('main.twitter_authorize', _external=True)
+    return twitter.authorize_redirect(redirect_uri)
+
+
+@main.route('/login/twitter/authorize')
+def twitter_authorize():
+    print("Hey")
+    twitter = oauth.create_client('twitter')
+    twitter.authorize_access_token()
+    resp = twitter.get('account/verify_credentials.json')
+    resp.raise_for_status()
+    profile = resp.json()
+    print("\n",profile,"\n")
+
+
+
+# Github login route
+@main.route('/login/github')
+def github_login():
+    github = oauth.create_client('github')
+    redirect_uri = url_for('main.github_authorize', _external=True)
+    return github.authorize_redirect(redirect_uri)
+
+
+# Github authorize route
+@main.route('/login/github/authorize')
+def github_authorize():
+    github = oauth.create_client('github')
+    github.authorize_access_token()
+    resp = github.get('user').json()
+    user_email = resp['email']
+    user = User.query.filter_by(email=user_email).first()
+    if user:
+        login_user(user)
+        return redirect(url_for('main.home'))
+    else:
+        user_obj = User(username=resp['name'],
+                        email=resp['email'],
+                        password=current_app.config["GOOGLE_CLIENT_SECRET"],
+                        is_active=True)
+
+        db.session.add(user_obj)
+        db.session.commit()
+        # user = User.query.filter_by(email=resp['email']).first()
+        profile = UserProfile(firstname=None, lastname=None, profile_image=resp['avatar_url'], birthday=None,
                               user_id=user_obj.id)
         db.session.add(profile)
         db.session.commit()
