f5a4f6c4214f59c58235a55de805976c2fe7dcd0
tirths.inexture <tirths.inexture@gmail.com>
2022-07-08 11:29:24 +0530

    update migrations

--- blogapp/models.py
+++ blogapp/models.py
@@ -54,7 +54,8 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(50), nullable=True)
     lastname = db.Column(db.String(50), nullable=True)
-    profile_image = db.Column(db.String(200), nullable=True, default='default.jpg')
+    profile_image = db.Column(db.String(200), nullable=True, default='http://res.cloudinary.com/dfmukiaes/image'
+                                                                     '/upload/v1657186897/Profile_images/ndo0gjrywo4yawbhxwqi.jpg')
     birthday = db.Column(db.Date, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
